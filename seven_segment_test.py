003ea108945b46f10c54991ad30db0f5ee446c6d
Sam <samworth27@gmail.com>
2023-05-11 17:03:14 +1000

    add sequential mode

--- seven_segment_test.py
+++ seven_segment_test.py
@@ -4,6 +4,10 @@ import numpy as np
 
 sevseg = SevenSegment(7, 11, 13, 15)
 
-for _ in range(60):
+for _ in range(20):
     sevseg.display(np.random.randint(0,10))
-    sleep(1)    
+    sleep(0.5)    
+
+for i in range(20):
+    sevseg.display(i % 10)
+    sleep(0.5)
