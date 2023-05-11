0b696c07ef09abb12d8d01a2345089e27bfac1af
Sam <samworth27@gmail.com>
2023-05-11 17:04:27 +1000

    changes to test

--- seven_segment_test.py
+++ seven_segment_test.py
@@ -4,10 +4,11 @@ import numpy as np
 
 sevseg = SevenSegment(7, 11, 13, 15)
 
-for _ in range(20):
-    sevseg.display(np.random.randint(0,10))
-    sleep(0.5)    
-
 for i in range(20):
     sevseg.display(i % 10)
-    sleep(0.5)
+    sleep(1)
+
+for _ in range(100):
+    sevseg.display(np.random.randint(0,10))
+    sleep(0.1)    
+
